e87f000466f5bdd98fc311e1570efcd0ffcff9d7
Kenny <xfinrodx@gmail.com>
2014-12-02 22:46:14 -0800

    Fixed default refresh time.
    Was 60 seconds for local debugging - now 10 minutes as intended.

--- FreeDnsService/Properties/Settings.Designer.cs
+++ FreeDnsService/Properties/Settings.Designer.cs
@@ -88,7 +88,7 @@ namespace FreeDnsService.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("60000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("600000")]
         public int RefreshMillis {
             get {
                 return ((int)(this["RefreshMillis"]));
